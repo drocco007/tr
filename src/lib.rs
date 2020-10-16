972a6ccdf1575e441a77c65b0a747f525374f598
Daniel Rocco <drocco@gmail.com>
2020-10-16 18:34:40 -0400

    Add docstring; remove unused import

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
-use std::io::Cursor;
 
 
 /// Create a mapping from each char in set1 to the corresponding char
@@ -20,6 +19,20 @@ pub fn map_charsets(set1: &str, set2: &str) -> HashMap<char, char> {
 }
 
 
+/// Extend s to length n by repeating the last char.
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!("Rust!!!", tr::rpad_last("Rust!", 7));
+/// ```
+///
+/// Returns s unmodified if n is <= s.len():
+///
+/// ```
+/// assert_eq!("why?", tr::rpad_last("why?", 0));
+/// assert_eq!("too small", tr::rpad_last("too small", 4));
+/// ```
 pub fn rpad_last<'a>(s: &'a str, n: usize) -> Cow<'a, str> {
     if s.len() < n {
         let mut buf = String::with_capacity(n);
