5e3dca86357639c31edd4c62a38b50a7a99a533b
Daniel Rocco <drocco@gmail.com>
2020-11-07 18:27:27 -0500

    Generalize command processing and add delete

--- src/command.rs
+++ src/command.rs
@@ -1,7 +1,10 @@
+use std::collections::HashSet;
+use std::io::{BufRead,Stdin,StdinLock,Write};
+
 use unicode_reader::CodePoints;
 
-use crate::arg_parser::Config;
-use crate::parser::map_charsets;
+use crate::arg_parser::{Config, parse_args};
+use crate::parser::{map_charsets, parse};
 
 
 pub fn show_help() {
@@ -14,18 +17,89 @@ pub fn show_version() {
 }
 
 
-pub fn translate(config: &Config) {
-    let map = map_charsets(&config.set1, &config.set2);
-
+pub fn process<F>(mut op: F) -> Result<(), std::io::Error>
+where
+    F: FnMut(u8) -> Option<u8>
+{
     let stdin = std::io::stdin();
+    let stdout = std::io::stdout();
 
-    let input = CodePoints::from(stdin.lock());
+    let mut stdin = stdin.lock();
+    let mut stdout = stdout.lock();
 
-    for c in input {
-        let c = c.unwrap();
+    let mut buffer = stdin.fill_buf()?;
+    let mut length = buffer.len();
 
-        let c = map.get(&c).unwrap_or(&c);
+    while !buffer.is_empty() {
+        for b in buffer {
+            match op(*b) {
+                Some(c) => { stdout.write(&[c])?; }
+                None => continue
+            }
+        }
 
-        print!("{}", c);
+        stdin.consume(length);
+        buffer = stdin.fill_buf().unwrap();
+        length = buffer.len();
     }
+
+    stdout.flush()?;
+
+    Ok(())
+}
+
+
+pub fn translate(config: &Config) -> Result<(), std::io::Error> {
+    // FIXME: duplicates map_charsets until we convert everything over
+    // to unicode
+    let (set1, set2) = (parse(&config.set1), parse(&config.set2));
+
+    let set2 = crate::parser::rpad_last(&set2, set1.len());
+
+    use std::collections::HashMap;
+    let map = set1.bytes().zip(set2.bytes()).collect::<HashMap<u8,u8>>();
+
+    process(|b| {
+        match map.get(&b) {
+            Some(&c) => Some(c),
+            _ => Some(b)
+        }
+    })
+}
+
+
+pub fn delete(config: &Config) -> Result<(), std::io::Error> {
+    let set = parse(&config.set1).bytes().collect::<HashSet<u8>>();
+
+    process(|b| match set.contains(&b) {
+        true => None,
+        false => Some(b)
+    })
+}
+
+
+pub fn tr<I>(args: I)
+where
+    I: IntoIterator,
+    I::Item: AsRef<str>
+{
+    let config = match parse_args(args) {
+        Err(message) => {
+            eprintln!("tr: {}", message);
+            std::process::exit(1);
+        },
+        Ok(config) => config
+    };
+
+    if config.help_requested {
+        show_help();
+    } else if config.version_requested {
+        show_version();
+    } else if config.delete {
+        delete(&config);
+    } else {
+        translate(&config);
+    }
+
+    std::process::exit(0);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
-use tr::command::{show_help, show_version, translate};
-use tr::arg_parser::parse_args;
+use tr::command::tr;
 
 
 fn main() {
-    let config = match parse_args(std::env::args()) {
-        Err(message) => {
-            eprintln!("tr: {}", message);
-            std::process::exit(1);
-        },
-        Ok(config) => config
-    };
-
-    if config.help_requested {
-        show_help();
-    } else if config.version_requested {
-        show_version();
-    } else {
-        translate(&config);
-    }
-
-    std::process::exit(0);
+    tr(std::env::args());
 }
